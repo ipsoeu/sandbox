4fd3cd0ce839df688463b801498238687ff3671f
Alessandro Dalla Benetta <alessandrodallabenetta@IMac-Pro.local>
2020-08-21 16:39:08 +0200

    updated

--- contact_tracing_apps/main.js
+++ contact_tracing_apps/main.js
@@ -19,5 +19,24 @@ $( document ).ready(function() {
         paging: false
     } );
 
+    console.log()
+    _.each(CHARTS, function(chart, index){
+        console.log(chart)
+        var $content = $('<div class="caption-section"></div>');
+        var $title = $('<h3></h3>').append(chart.app_name);
+        var $subtitle = $('<h5>Temporal evolution of users activity, DATA tweets</h5>'.replace('DATA', chart['number_of_tweets']))
+        var $chart_1 = $('<div style="text-align: center;margin-bottom: 10px;"></div>');
+        $chart_1.append($('<img style="width: 100%" ></img>').attr('src',"contact_tracing_apps/" + chart['temporal_evolution'] ));
+        var $chart_2 = $('<div style="text-align: center;margin-bottom: 10px;"></div>');
+        $chart_2.append($('<img style="width: 100%" ></img>').attr('src',"contact_tracing_apps/" + chart['lang'] ))
+        
+        $content.append($title)
+        $content.append($subtitle)
+        $content.append($chart_1);
+        $content.append($chart_2);
+
+
+        this.append($content)
+    },$('#charts'))
 
 });
