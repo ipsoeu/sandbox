24fa5e8397d7fef388cd06f1e76f5f0296468710
Alessandro Dalla Benetta <alessandrodallabenetta@IMac-Pro.local>
2020-11-12 15:14:20 +0100

    n of tweets

--- contact_tracing_apps/main.js
+++ contact_tracing_apps/main.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
 
     $('#total_tweets').append(values.reduce(function(a, b) { return a + b; }, 0))
 
+    $('#sa_n_of_tweets').append(values['SA_N_TWEETS']);
+
     data_sets = []
     for (var key in DATA) {
         data_sets.push([key, DATA[key]])
