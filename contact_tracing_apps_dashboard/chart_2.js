b210113c869f063d97a24e7a120bced7c8a1aab2
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-02-23 17:11:48 +0100

    fixed

--- contact_tracing_apps_dashboard/chart_2.js
+++ contact_tracing_apps_dashboard/chart_2.js
@@ -9,24 +9,37 @@ $(document).ready(function () {
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var y = d3.scaleBand()			// x = d3.scaleBand()	
-        .rangeRound([0, height])	// .rangeRound([0, width])
-        .paddingInner(0.05)
-        .align(0.1);
+    // var y = d3.scaleBand()			// x = d3.scaleBand()	
+    //     .rangeRound([0, height])	// .rangeRound([0, width])
+    //     .paddingInner(0.05)
+    //     .align(0.1);
 
-    var x = d3.scaleLinear()		// y = d3.scaleLinear()
-        .rangeRound([0, width]);	// .rangeRound([height, 0]);
+    // var x = d3.scaleLinear()		// y = d3.scaleLinear()
+    //     .rangeRound([0, width]);	// .rangeRound([height, 0]);
+
+    x = d3.scaleBand()
+    
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+
+    y = d3.scaleLinear()
+    //.domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+    .rangeRound([height - margin.bottom, margin.top])
 
     var z = d3.scaleOrdinal()
         .range(["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51" ,"#1d3557"]);
 
     var keys = MOBILE_APPS;
 
-    y.domain(data.map(function (d) { return d.datetime; }));
-    x.domain([0, d3.max(data, function (d) {return d.total;})]).nice();
+    // y.domain(data.map(function (d) { return d.datetime; }));
+    // x.domain([0, d3.max(data, function (d) {return d.total;})]).nice();
+    x.domain( MOBILE_APPS_DATES );
+    
+    y.domain([0, d3.max(data, function (d) {return d.total;})]) //.nice();
+
     z.domain(keys);
     
-    console.log(d3.stack().keys(keys)(data));
+    console.log(x("2020-07-24"))
 
 
 
@@ -38,15 +51,22 @@ $(document).ready(function () {
         .selectAll("rect")
         .data(function (d) { return d; })
         .enter().append("rect")
-        .attr("y", function (d) { return y(d.data.datetime); })	    //.attr("x", function(d) { return x(d.data.State); })
-        .attr("x", function (d) { return x(d[0]); })			    //.attr("y", function(d) { return y(d[1]); })	
-        .attr("width", function (d) { a = d[1] === NaN ? 0: d[1];return x(a) - x(d[0]); })	//.attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("height", y.bandwidth());						    //.attr("width", x.bandwidth());	
+        .attr("x", function (d) { return x(d.data.datetime); })	    //.attr("x", function(d) { return x(d.data.State); })
+        .attr("y", function (d) { a = d[1] === NaN ? 0: d[1]; return y(a); })			    //.attr("y", function(d) { return y(d[1]); })	
+        .attr("height", function (d) { 
+            a = d[1] === NaN ? 0: d[1];
+            b = d[0] === NaN ? 0: d[0];
 
-    g.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0,0)") 						//  .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisLeft(y));									//   .call(d3.axisBottom(x));
+            return y(b) - y(a); })	//.attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width", x.bandwidth());						    //.attr("width", x.bandwidth());	
+
+        // .attr("width", function (d) { a = d[1] === NaN ? 0: d[1];return x(a) - x(d[0]); })	//.attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        // .attr("height", y.bandwidth());						    //.attr("width", x.bandwidth());	
+
+    // g.append("g")
+    //     .attr("class", "axis")
+    //     .attr("transform", "translate(0,0)") 						//  .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisLeft(y));									//   .call(d3.axisBottom(x));
 
     // // g.append("g")
     // //     .attr("class", "axis")
