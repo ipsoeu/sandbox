2d055e76720e3da18bbfecff5e070decd698bdf2
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-07-09 17:11:23 +0200

    added community table

---
+++ contact_tracing_apps_hashtags/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+  
+    $('#community_table').DataTable({
+        data: HASHTAGS_COMMUNITIES,
+        columns: [
+            { data: "hashtags_count", title: "Number of Hashtags" },
+
+            { data: "total_tweets", title: 'Number of Tweets' },
+            { data: "community_isolation_by_tweets", title: 'Isolation index' },
+          
+        ],
+        order: [[0, "desc"]],      
+    });
+} );
+
+//dict_keys(['tweets_ids', 'hashtags', 'total_tweets', 'isolated_tweets', 'shared_tweets', 'community_isolation_by_tweets', 'isolated_geolocalized_tweets', 'isolated_mobile_apps', 'isolated_neutral_tweets', 'isolated_positive_tweets', 'isolated_negative_tweets', 'isolated_emm_news', 'geolocalized_tweets', 'mobile_apps', 'neutral_tweets', 'positive_tweets', 'negative_tweets', 'emm_news'])
+//https://stackoverflow.com/questions/39617207/datatables-set-complex-headers-from-javascript
