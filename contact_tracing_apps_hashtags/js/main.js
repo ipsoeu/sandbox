a8e28621943ec8be7f333034cebd0ba79ff4c18f
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-07-14 11:49:52 +0200

    density description

--- contact_tracing_apps_hashtags/js/main.js
+++ contact_tracing_apps_hashtags/js/main.js
@@ -22,6 +22,11 @@ $(document).ready(function() {
         ],
         order: [[2, "desc"]],      
     });
+
+    $('#network_density').append(NETWORK_INFO['density']);
+    $('#network_nodes').append(NETWORK_INFO['nodes']);
+    $('#network_edges').append(NETWORK_INFO['edges']);
+
 } );
 
 //dict_keys(['tweets_ids', 'hashtags', 'total_tweets', 'isolated_tweets', 'shared_tweets', 'community_isolation_by_tweets', 'isolated_geolocalized_tweets', 'isolated_mobile_apps', 'isolated_neutral_tweets', 'isolated_positive_tweets', 'isolated_negative_tweets', 'isolated_emm_news', 'geolocalized_tweets', 'mobile_apps', 'neutral_tweets', 'positive_tweets', 'negative_tweets', 'emm_news'])
