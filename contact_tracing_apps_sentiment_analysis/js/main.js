a72af68703f25a35f2789e83af4c76620affdde5
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-09-01 16:50:00 +0200

    sa

--- contact_tracing_apps_sentiment_analysis/js/main.js
+++ contact_tracing_apps_sentiment_analysis/js/main.js
@@ -6,10 +6,11 @@ $(document).ready(function () {
             { data: function (e) { return e.app_name.slice(0, 2); }, title: "Country" },
             { data: function (e) { return e.app_name.slice(3).replace('_', ' '); }, title: "App name" },
             { title: "Total tweets", data: 'total_tweets' },
-            { title: "Classified tweets", data: 'classified_tweets' },
+            
             { title: "Neutral tweets (%)", data: 'neutral_tweets_relative' },
             { title: "Positive tweets (%)", data: 'positive_tweets_relative' },
             { title: "Negative tweets (%)", data: 'negative_tweets_relative' },
+            { title: "Unclassified tweets (%)", data: 'unclassified_tweets_relative' },
         ],
         order: [[2, "desc"]],
         paging: false
