6e9aaa4f3d67901b1f0f27adf37fb61865709087
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-09-01 15:19:19 +0200

    sa doc

--- contact_tracing_apps_basic_information/data/sa.js
+++ contact_tracing_apps_basic_information/data/sa.js
@@ -1,3 +1,3 @@
-var SA_DATA = [["it_immuni", 153615, 92252, 14197, 34705], ["ie_covid_tracker_ireland", 101820, 45086, 14061, 32750], ["fr_stopcovid", 94370, 11783, 30128, 38428], ["de_corona_warn_app", 90710, 46839, 5010, 31520], ["es_radar_covid", 73645, 35325, 7705, 23910], ["uk_nhs_covid_19_app", 37077, 17126, 7190, 11015], ["ch_swisscovid", 6825, 3591, 947, 1860], ["cz_erouska", 2694, 45, 20, 15], ["no_smittestopp", 2134, 94, 42, 32], ["at_stopp_corona", 1879, 935, 93, 744]]; 
+// var SA_DATA = [["it_immuni", 153615, 92252, 14197, 34705], ["ie_covid_tracker_ireland", 101820, 45086, 14061, 32750], ["fr_stopcovid", 94370, 11783, 30128, 38428], ["de_corona_warn_app", 90710, 46839, 5010, 31520], ["es_radar_covid", 73645, 35325, 7705, 23910], ["uk_nhs_covid_19_app", 37077, 17126, 7190, 11015], ["ch_swisscovid", 6825, 3591, 947, 1860], ["cz_erouska", 2694, 45, 20, 15], ["no_smittestopp", 2134, 94, 42, 32], ["at_stopp_corona", 1879, 935, 93, 744]]; 
 
-var TIMESTAMP = "2021/8/4";
+// var TIMESTAMP = "2021/9/1";
--- contact_tracing_apps_sentiment_analysis/js/main.js
+++ contact_tracing_apps_sentiment_analysis/js/main.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
 
-    
-
     $('#sa_table').DataTable({
         data: SA_DATA_TABLE_RELATIVE,
         columns: [
-            { data: function(e){return e.app_name.slice(0,2);}, title: "Country" },
-            { data: function(e){return e.app_name.slice(3).replace('_', ' ');}, title: "App name" },
+            { data: function (e) { return e.app_name.slice(0, 2); }, title: "Country" },
+            { data: function (e) { return e.app_name.slice(3).replace('_', ' '); }, title: "App name" },
             { title: "Number of tweets", data: 'n_of_tweets' },
             { title: "Neutral tweets (%)", data: 'neutral' },
             { title: "Positive tweets (%)", data: 'positive' },
@@ -15,5 +13,5 @@ $(document).ready(function () {
         order: [[2, "desc"]],
         paging: false
     });
-    
+
 });
