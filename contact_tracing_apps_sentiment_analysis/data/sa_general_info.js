8dd1d66a9325a09b0a158caaac0e65658cdd8d78
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-09-06 14:54:25 +0200

    sa general

---
+++ contact_tracing_apps_sentiment_analysis/data/sa_general_info.js
@@ -0,0 +1,4 @@
+var SA_GENERAL_INFO = {"total_tweets": 568630, "neutral_tweets": 253282, "negative_tweets": 175017, "positive_tweets": 79466, "no_classified_tweets": 60865}; 
+
+var LANGS_PIECHART = [["English", 174190], ["Italian", 120533], ["German", 84756], ["Spanish", 66700], ["French", 61586], ["Undefined", 36344], ["Others", 12591]]; 
+
