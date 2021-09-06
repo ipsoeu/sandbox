aa45968e3bd198fc7c5f5e2789c4613cf015848f
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-09-06 17:08:44 +0200

    sentimetn analysis

--- contact_tracing_apps_sentiment_analysis/data/sa_general_info.js
+++ contact_tracing_apps_sentiment_analysis/data/sa_general_info.js
@@ -1,4 +1,4 @@
-var SA_GENERAL_INFO = {"total_tweets": 568630, "neutral_tweets": 253282, "negative_tweets": 175017, "positive_tweets": 79466, "no_classified_tweets": 60865}; 
+var SA_GENERAL_INFO = {"total_tweets": 568630, "neutral_tweets": 253282, "neutral_tweets_relative": 44, "negative_tweets": 175017, "negative_tweets_relative": 30, "positive_tweets": 79466, "positive_tweets_relative": 13, "no_classified_tweets": 60865, "no_classified_tweets_relative": 13}; 
 
-var LANGS_PIECHART = [["English", 174190], ["Italian", 120533], ["German", 84756], ["Spanish", 66700], ["French", 61586], ["Undefined", 36344], ["Others", 12591]]; 
+var LANGS_PIECHART = {"English": 174190, "Italian": 120533, "German": 84756, "Spanish": 66700, "French": 61586, "Undefined": 36344, "Others": 12591}; 
 
--- contact_tracing_apps_sentiment_analysis/js/main.js
+++ contact_tracing_apps_sentiment_analysis/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+
+    $('#total_tweets').append(SA_GENERAL_INFO['total_tweets']);
+    $('#neutral_tweets_relative').append(SA_GENERAL_INFO['neutral_tweets_relative']);
+    $('#neutral_tweets').append(SA_GENERAL_INFO['neutral_tweets']);
+
+    $('#no_classified_tweets_relative').append(SA_GENERAL_INFO['no_classified_tweets_relative']);
+    $('#no_classified_tweets').append(SA_GENERAL_INFO['no_classified_tweets']);
+
+
+    $('#no_classified_tweets').append(SA_GENERAL_INFO['no_classified_tweets']);
+
+    $('#others_tweet').append(LANGS_PIECHART['Others'])
+    $('#undefined_tweet').append(LANGS_PIECHART['Undefined'])
+    
+    $('#negative_tweets').append(SA_GENERAL_INFO['negative_tweets']);
+    $('#positive_tweets').append(SA_GENERAL_INFO['positive_tweets']);
+
+    $('#with_opinions').append(SA_GENERAL_INFO['positive_tweets_relative'] + SA_GENERAL_INFO['negative_tweets_relative'])
     var data = JSON.parse(JSON.stringify(SA_BY_APP));
     $('#sa_table').DataTable({
         data: data,
