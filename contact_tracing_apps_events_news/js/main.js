495cbe12eadd9b67f34effe0eafff705d6194d84
DALLA BENETTA Alessandro <alessandro.dalla-benetta@ext.ec.europa.eu>
2021-12-17 10:22:37 +0100

    tables

--- contact_tracing_apps_events_news/js/main.js
+++ contact_tracing_apps_events_news/js/main.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function () {
 
-
-
-
-
     var top_events = JSON.parse(JSON.stringify(TEMPORAL_EVOLUTION_BY_APP));
+
+    var positive_top_events = JSON.parse(JSON.stringify(TEMPORAL_EVOLUTION_BY_APP_POSITIVE));
+
+    var negative_top_events = JSON.parse(JSON.stringify(TEMPORAL_EVOLUTION_BY_APP_NEGATIVE));
 
     top_events = top_events.sort((a, b) => b.zscore - a.zscore)
 
@@ -13,6 +13,58 @@ $(document).ready(function () {
     
     $("#events_by_zscore").DataTable({
         data: top_events.slice(0, 50),
+        columns: [
+            { data: "datetime_str", title: "Date" },
+            { data: "zscore", title: "Z-score index" },
+            { data: "total", title: "Number of tweets" },
+            {data:function (d){
+                record = JSON.parse(JSON.stringify(d));
+                delete record['datetime']
+                delete record['datetime_str']
+                keys = Object.keys(record).sort(function(a,b){return record[b] - record[a]})
+                
+                
+                return `${keys[1]}(${record[keys[1]]}), ${keys[2]}(${record[keys[2]]}), ${keys[3]}(${record[keys[3]]}), `
+            },
+            title: 'Main mobile apps'}
+
+        ],
+        order: [[1, "desc"]],
+        columnDefs: [{
+            targets: [1],
+            render: $.fn.dataTable.render.number(',', '.', 3)
+        }],
+        // 'paging': false
+    });
+
+    $("#positive_events_by_zscore").DataTable({
+        data: positive_top_events.slice(0, 50),
+        columns: [
+            { data: "datetime_str", title: "Date" },
+            { data: "zscore", title: "Z-score index" },
+            { data: "total", title: "Number of tweets" },
+            {data:function (d){
+                record = JSON.parse(JSON.stringify(d));
+                delete record['datetime']
+                delete record['datetime_str']
+                keys = Object.keys(record).sort(function(a,b){return record[b] - record[a]})
+                
+                
+                return `${keys[1]}(${record[keys[1]]}), ${keys[2]}(${record[keys[2]]}), ${keys[3]}(${record[keys[3]]}), `
+            },
+            title: 'Main mobile apps'}
+
+        ],
+        order: [[1, "desc"]],
+        columnDefs: [{
+            targets: [1],
+            render: $.fn.dataTable.render.number(',', '.', 3)
+        }],
+        // 'paging': false
+    });
+
+    $("#negative_events_by_zscore").DataTable({
+        data: negative_top_events.slice(0, 50),
         columns: [
             { data: "datetime_str", title: "Date" },
             { data: "zscore", title: "Z-score index" },
